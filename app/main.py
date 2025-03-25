7a3c426dc53903dc65e737ee8dd324f7a0072704
DylGit3 <senddylmail27@gmail.com>
2025-03-24 17:30:27 -0700

    Initial additions

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import sqlite3
+import random
+import string
+
+
+def main():
+    print('Hello')
+
+
+if __name__ == '__main__':
+    main()
